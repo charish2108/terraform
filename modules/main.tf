0113dd6113e1b7c019f51b9e61d8537dc7f687d5
charish2108 <charish.2108@gmail.com>
2022-08-23 16:37:37 +0530

    updated changes

--- modules/main.tf
+++ modules/main.tf
@@ -5,21 +5,27 @@ provider "aws" {
 }
 
 module "project1" {
-  source = "./modules/vpc"
+  # source = "./modules/vpc"   # github repo path where main.rf and variable.tf live
+  source = "github.com/charish2108/terraform/tree/main/modules/vpc"
   vpcname = "DevOpsTest"
   igwname = "DevOpsTest-IGW"
 }
 
-module "project2" {
-  source = "./modules/vpc"
+module "project2" {         
+  # source = "./modules/vpc"  # using GitHub SSH
+  # source = "git@github.com:charish2108/terraform.git" # Private key should be added for this
+  source = "github.com/charish2108/terraform/tree/main/modules/vpc"
   vpcname = "DevOpsTest2"
   igwname = "DevOpsTest2-IGW"
 }
 
 module "project3" {
-  source = "./modules/vpc"
+  # source = "./modules/vpc"  # github repo path where main.rf and variable.tf live
+  source = "github.com/charish2108/terraform/tree/main/modules/vpc"
   vpcname = "DevOpsTest3"
   igwname = "DevOpsTest3-IGW"
 }
 
 # terraform init
+# terraform plan
+# terraform apply --auto-approve
